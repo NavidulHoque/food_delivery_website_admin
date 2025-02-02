0bc53634c85375c0066e38c83bd980f76528b249
navidul hoque <hnavidul@gmail.com>
2025-02-02 16:25:14 +0600

    admin

--- app/(root)/actions/image.ts
+++ app/(root)/actions/image.ts
@@ -4,7 +4,7 @@ import axios from "axios"
 
 export async function generateImageURL(formData: FormData): Promise<string> {
 
-    const imageResponse = await axios.post(`https://api.imgbb.com/1/upload?key=${process.env.NEXT_PUBLIC_IMGBB_API_KEY}`, formData)
+    const imageResponse = await axios.post(`https://api.imgbb.com/1/upload?key=${process.env.IMGBB_API_KEY}`, formData)
 
     return imageResponse.data.data.url
 }
